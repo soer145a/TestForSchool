1ea0c54aea7e11ae6b53a9da92f458c9896101fb
soer145a <36399126+soer145a@users.noreply.github.com>
2020-09-03 18:33:46 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+function fireCheck() { //Fired from button
+  const int1 = input1;
+  const int2 = input2;
+  const int3 = input3;
+  calcTotal(int1, int2, int3);
+}
+function calcTotal(a, b, c) { //Calculator for product
+  const display = a + b + c;
+    console.log(display);
+  return display;
+}
+//output to test performance
+module.exports = calcTotal;
