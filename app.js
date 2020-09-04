e0e35ebb041acd9515cff574a821601e7c1696da
soer145a <soren.remboll@gmail.com>
2020-09-04 11:20:22 +0200

    Change in HMTL

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-function fireCheck() { //Fired from button
+function fireCheck() { // Fired from button
   const int1 = input1.value;
   const int2 = input2.value;
   const int3 = input3.value;
   calcTotal(int1, int2, int3);
 }
-function calcTotal(a, b, c) { //Calculator for product
+function calcTotal(a, b, c) { // Calculator for product
   const display = a + b + c;
   const target = document.querySelector("#target");
   target.textContent = display;
   return display;
 }
-//output to test performance
+// output to test performance
 module.exports = calcTotal;
